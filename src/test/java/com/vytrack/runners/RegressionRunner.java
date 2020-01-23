80f50cf9a47f154699969731e4107eaf350c5835
Bobojon4450 <bobojon.rva@gmail.com>
2020-01-22 21:14:20 -0500

    changed the pom.xml

--- src/test/java/com/vytrack/runners/RegressionRunner.java
+++ src/test/java/com/vytrack/runners/RegressionRunner.java
@@ -8,7 +8,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = {
                 "src/test/resources/features/activities",
-                "src/test/resources/features/fleet"
+                "src/test/resources/features/fleet",
+                "src/test/resources/features/Login.feature"
         },
         glue = "com/vytrack/step_definitions",
         dryRun = false,
